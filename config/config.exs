027188e77375a12a11093cf5904080b3750321d1
TaiO1 <53664537+TaiO1@users.noreply.github.com>
2021-11-01 16:38:46 -0600

    Final review

--- config/config.exs
+++ config/config.exs
@@ -2,20 +2,8 @@
 # and its dependencies with the aid of the Mix.Config module.
 use Mix.Config
 
-# Define your application's host and port (defaults to "http://localhost:4001")
-#config :hound, app_host: "http://localhost", app_port: 4000
-
 # Start with selenium driver (default)
-config :hound, driver: "phantomjs"
-
-# Use Chrome with the default driver (selenium)
-#config :hound, browser: "chrome"
-
-# Start with default driver at port 1234 and use firefox
-#config :hound, port: 1234, browser: "firefox"
-
-# Start Hound for PhantomJs
 #config :hound, driver: "phantomjs"
 
 # Start Hound for ChromeDriver (default port 9515 assumed)
-#config :hound, driver: "chrome_driver"
+config :hound, driver: "chrome_driver"
--- test/homework_test.exs
+++ test/homework_test.exs
@@ -1,22 +1,7 @@
-defmodule ForgotPassword do  
-  # Screenshot on failure macro. Sends png to screenshots folder, also sets window size to 1920 x 1080 before capture 
-  defmacro assert_sof(assertion) do
-    quote do
-      try do
-        assert unquote(assertion)
-      rescue
-         error ->
-          set_window_size current_window_handle(), 1920, 1080
-          take_screenshot("screenshots/forgot_password.png")
-          raise error
-      end
-    end
-  end
-
+defmodule ForgotPassword do
   # Import helpers
   use Hound.Helpers
   use ExUnit.Case
-  
   # Start hound session and destroy when tests are run
   hound_session()
   
@@ -28,25 +13,25 @@ defmodule ForgotPassword do
     forgot_password_nav = find_within_element(content, :xpath, ~s|//a[contains(text(),"Forgot Password")]|)
 
     forgot_password_nav |> click()
-    assert_sof current_url() == "https://the-internet.herokuapp.com/forgot_password"
+    assert current_url() == "https://the-internet.herokuapp.com/forgot_password"
 
     # Should input value into field, add additional text. Verify each exists after input 
     email = find_element(:id, "email")
     retrieve_password = find_element(:id, "form_submit")
 
     input_into_field(email, "test@")
-    assert_sof attribute_value(email, "value") == "test@"
+    assert attribute_value(email, "value") == "test@"
     input_into_field(email, "test.org")
-    assert_sof attribute_value(email, "value") == "test@test.org"
+    assert attribute_value(email, "value") == "test@test.org"
 
     # Submit email, confirm redirect with text from new page
     retrieve_password |> click()
-    assert_sof visible_page_text() =~ "Internal Server Error"
+    assert visible_page_text() =~ "Internal Server Error"
 
     # Navigate back a page after redirect, confirm email field stays populated
     navigate_back()
     input = find_element(:id, "email")
-    assert_sof attribute_value(input, "value") == "test@test.org"
+    assert attribute_value(input, "value") == "test@test.org"
 
     delete_cookies()
   end
@@ -89,7 +74,7 @@ defmodule HoverTest do
 end
 
 defmodule Geolocation do
-  # Screenshot on failure macro. Sends png to screenshots folder, also sets window size to 1920 x 1080 before capture 
+  # Screenshot on failure macro. Test set to capture screenshot on either success or failure
   defmacro assert_tfs(assertion) do
   quote do
     try do
@@ -97,12 +82,11 @@ defmodule Geolocation do
       rescue
          error ->
           set_window_size current_window_handle(), 1920, 1080
-          take_screenshot("screenshots/geolocation_failure.png")
+          take_screenshot("__screenshots/failure.png")
           raise error
       end
     end
   end  
-  
   # Import helpers
   use Hound.Helpers
   use ExUnit.Case
@@ -111,8 +95,8 @@ defmodule Geolocation do
   hound_session()
 
   test "Geolocation" do
+    delete_cookies()
     navigate_to "https://the-internet.herokuapp.com/"
-
     # Find Geolocation, navigate to new page and confirm with URL 
     content = find_element(:id, "content")
     geolocation_nav = find_within_element(content, :xpath, ~s|//a[contains(text(),"Geolocation")]|)
@@ -120,29 +104,30 @@ defmodule Geolocation do
     geolocation_nav |> click()
     assert_tfs current_url() == "https://the-internet.herokuapp.com/geolocation"
 
-    # Attempt to grant browser location permission
+    # Attempt to access location
     where_am_i = find_element(:xpath, ~s|//button[contains(text(),"Where am I?")]|)
-
     where_am_i |> click()
-    
-    send_keys :shift
+    # Pass keys to approve request modal
     send_keys :tab
-    send_keys :null
-
+    send_keys :tab
     send_keys :enter
-    
-    # PhantomJS does not support geolocation
-    # If PhantomJS is in use, geolocation_failure screenshot should be added to screenshots folder
-    # If Chromedriver is in use, geolocation_success screenshot should be added to screenshots folder. Should show your latitude and longitude
-    assert_tfs visible_page_text() =~ "Latitude:"
 
-    # View current location on Google Maps
-    see_on_google = find_element(:xpath, ~s|//div[contains(text(),"See it on Google")]|)
-    see_on_google |> click
+    # Chromedriver supports geolocation, should pass this portion
+    # PhantomJS does not support geolocation. Redirect screen will be captured as fail state
+    assert_tfs visible_page_text() =~ "Where am I?"
 
+    # Not a fan of hard coded waits, but the driver continually failed the test as the element didn't load fast enough.
+    Process.sleep(50)
+    # Navigate to Google Maps, screenshot current location
+    assert_tfs visible_page_text() =~ "See it on Google"
+    see_map = find_element(:xpath, ~s|//a[contains(text(),"See it on Google")]|)
+    see_map |> click()
+    assert current_url() =~ "www.google.com"
+
+    # Set window size, and allow pause for Map to render before screenshot
     set_window_size current_window_handle(), 1920, 1080
-    take_screenshot("screenshots/geolocation_success.png")
-    
-    delete_cookies()
+    Process.sleep(50)
+    # Screenshot on sucess. Test set to capture screenshot on either success or failure
+    take_screenshot("__screenshots/Success!.png")
   end 
 end    
