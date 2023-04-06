1fad6ba9664ac31930455a99e37d257c001f6867
TaiO1 <53664537+TaiO1@users.noreply.github.com>
2023-04-05 21:39:14 -0600

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Homework.MixProject do
     [
       app: :homework,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.8.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
